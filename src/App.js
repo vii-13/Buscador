f2911b9bad5740d8aefdbf51b7c272d277bf59d9
Kency <Kencylantigua027@gmail.com>
2021-04-15 18:56:10 -0400

    Setup sing in with Google

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import Buscador from './componentes/Buscador';
 
+import {
+  db,
+  googleAuthProvider,
+  firebase
+} from './componentes/config/firebase';
+import Button from './componentes/Button';
+
+
 class App extends Component {
   
   state = {
@@ -38,5 +46,37 @@ class App extends Component {
     );
   }
   }
+
+ 
+function app () {
+  const signInWithGoogle = async () => {
+
+
+    // Coloca el lenguaje de preferencia del dispositivo
+    firebase.auth().useDeviceLanguage();
+    
+    // Inicio el procedo de login dentro de un try ... catch
+    
+    try {
+    await firebase.auth().signInWithPopup(googleAuthProvider);
+    
+    } catch (e) {
+    
+    console.error(e.message);
+    
+    }
+    return(
+    <div>
+      <Button onClick={signInWithGoogle} > Sing in with Google</Button>
+    </div> 
+    );
+     
+    }
+
+
+
+}
+
+
 
 export default App;
