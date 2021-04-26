97f3d2ab6b723f63af77bb8289855b352872a069
Esterlyn <diazesterlin62@gmail.com>
2021-04-26 12:50:24 -0400

    montando el proyecto a gh-pages

--- src/App.js
+++ src/App.js
@@ -3,93 +3,92 @@ import Buscador from './componentes/Buscador';
 import Resultado from './componentes/Resultado';
 
 class App extends Component {
-  
+
   state = {
-    termino:'',
+    termino: '',
     imagenes: [],
     pagina: ''
   }
   scroll = () => {
     const elemento = document.querySelector('.jumbotron');
-    elemento.scrollIntoView('smooth','start')
+    elemento.scrollIntoView('smooth', 'start')
   }
-paginaAnterior = () => {
- //Leer el state de la pagina actual
- let pagina = this.state.pagina;
- // Resta uno a la pagina actual
+  paginaAnterior = () => {
+    //Leer el state de la pagina actual
+    let pagina = this.state.pagina;
+    // Resta uno a la pagina actual
 
- //Si la pagina es 1 ya no era asi atras :v
- if(pagina === 1) return null;
- pagina-- ;
- //Agregar el cambio al state
- this.setState({
-   pagina
- }, () => {
-  this.consultarApi();
-  this.scroll();
-});
+    //Si la pagina es 1 ya no era asi atras :v
+    if (pagina === 1) return null;
+    pagina--;
+    //Agregar el cambio al state
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    });
 
- //console.log(pagina);
+    //console.log(pagina);
 
-}
-paginaSiguiente = () => {
- //Leer el state de la pagina actual
- let pagina = this.state.pagina;
- // Sumar uno a la pagina actual
- pagina++ ;
- //Agregar el cambio al state
- this.setState({
-   pagina
- }, () => {
-  this.consultarApi();
-  this.scroll();
-});
- //console.log(pagina);
-}
+  }
+  paginaSiguiente = () => {
+    //Leer el state de la pagina actual
+    let pagina = this.state.pagina;
+    // Sumar uno a la pagina actual
+    pagina++;
+    //Agregar el cambio al state
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    });
+    //console.log(pagina);
+  }
 
   consultarApi = () => {
     const termino = this.state.termino;
     const pagina = this.state.pagina;
     const URL = `https://pixabay.com/api/?key=21166249-df6dd5dc586305b77e6a6b598&q=${termino}&
     per_page=30&page=${pagina}`;
-
-    fetch(URL)
-    .then(respuesta => respuesta.json() )
-    .then(resultado => this.setState({ imagenes : resultado.hits }) )
-    }
+       fetch(URL)
+      .then(respuesta => respuesta.json())
+      .then(resultado => this.setState({ imagenes: resultado.hits }))
+  }
 
   datosBusqueda = (termino) => {
     this.setState({
-      termino : termino,
-      pagina : 1
+      termino: termino,
+      pagina: 1
     }, () => {
       this.consultarApi();
     })
   }
-  render(){
+  render() {
     return (
       <div className="container">
         <div className="jumbotron">
-        <div>
-          <br/>
-          <p className="lead text-center fs-1 fw-bold">Buscador de Imagenes</p>
-          <br/>
-          <Buscador  
-                  datosBusqueda={this.datosBusqueda}
-          />
-        </div>
-        <div className="row content-center">
-              <Resultado 
-                 imagenes={this.state.imagenes}
-                 paginaAnterior={this.paginaAnterior}
-                 paginaSiguiente={this.paginaSiguiente}
-                />
-                <p className="text-center fw-bold">PARA VER RECOMENDACIONES HAS CLICK EN SIGUIENTE</p>
-        </div>
+          <div>
+            <br />
+            <p className="lead text-center fs-1 fw-bold">Buscador de Imagenes</p>
+            <br />
+            <Buscador
+              datosBusqueda={this.datosBusqueda}
+            />
+          </div>
+          <div className="row content-center">
+            <Resultado
+              imagenes={this.state.imagenes}
+              paginaAnterior={this.paginaAnterior}
+              paginaSiguiente={this.paginaSiguiente}
+            />
+
+          </div>
         </div>
       </div>
     );
   }
-  }
+}
 
 export default App;
