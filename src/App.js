b3af78c4cb1047ff3206b99eb62cc4d8e360dbf6
Esterlyn <diazesterlin62@gmail.com>
2021-04-30 14:47:29 -0400

    css + formato

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
-import Buscador from './componentes/Buscador';
-import Resultado from './componentes/Resultado';
-import {db, googleAuthProvider,firebase} from './config/firebase';
-import Button from './componentes/Button';
-import BotonSubir from './componentes/BotonSubir';
-
-
+import React, { Component } from "react";
+import Buscador from "./componentes/Buscador";
+import Resultado from "./componentes/Resultado";
+import { db, googleAuthProvider, firebase } from "./config/firebase";
+import Button from "./componentes/Button";
+import BotonSubir from "./componentes/BotonSubir";
 
 class App extends Component {
   state = {
-    termino: '',
+    termino: "",
     imagenes: [],
-    pagina: ''
-  }
+    pagina: "",
+  };
 
   scroll = () => {
-    const elemento = document.querySelector('.jumbotron');
-    elemento.scrollIntoView('smooth', 'start')
-  }
-  
+    const elemento = document.querySelector(".container");
+    elemento.scrollIntoView("smooth", "start");
+  };
+
   paginaAnterior = () => {
     //Leer el state de la pagina actual
     let pagina = this.state.pagina;
@@ -28,16 +26,16 @@ class App extends Component {
     if (pagina === 1) return null;
     pagina--;
     //Agregar el cambio al state
-    this.setState({
-      pagina
-    }, () => {
-      this.consultarApi();
-      this.scroll();
-    });
-
-    //console.log(pagina);
-
-  }
+    this.setState(
+      {
+        pagina,
+      },
+      () => {
+        this.consultarApi();
+        this.scroll();
+      }
+    );
+  };
 
   paginaSiguiente = () => {
     //Leer el state de la pagina actual
@@ -45,50 +43,52 @@ class App extends Component {
     // Sumar uno a la pagina actual
     pagina++;
     //Agregar el cambio al state
-    this.setState({
-      pagina
-    }, () => {
-      this.consultarApi();
-      this.scroll();
-    });
-    //console.log(pagina);
-  }
+    this.setState(
+      {
+        pagina,
+      },
+      () => {
+        this.consultarApi();
+        this.scroll();
+      }
+    );
+  };
 
   consultarApi = () => {
     const termino = this.state.termino;
     const pagina = this.state.pagina;
     const URL = `https://pixabay.com/api/?key=21166249-df6dd5dc586305b77e6a6b598&q=${termino}&
     per_page=30&page=${pagina}`;
-       fetch(URL)
-      .then(respuesta => respuesta.json())
-      .then(resultado => this.setState({ imagenes: resultado.hits }))
-  }
+    fetch(URL)
+      .then((respuesta) => respuesta.json())
+      .then((resultado) => this.setState({ imagenes: resultado.hits }));
+  };
 
   datosBusqueda = (termino) => {
-    this.setState({
-      termino: termino,
-      pagina: 1
-    }, () => {
-      this.consultarApi();
-    })
-  }
+    this.setState(
+      {
+        termino: termino,
+        pagina: 1,
+      },
+      () => {
+        this.consultarApi();
+      }
+    );
+  };
 
-  
   render() {
     return (
-
       <div className="container">
+        <div className="menu">
+          <BotonSubir />
+          <p className="lead text-center fs-1 fw-bold">Buscador de Imagenes</p>
 
-          <div className="menu">
-          <BotonSubir/>
-            <p className="lead text-center fs-1 fw-bold">Buscador de Imagenes</p>
-            
-            <Buscador datosBusqueda={this.datosBusqueda}/>
-          </div>
+          <Buscador datosBusqueda={this.datosBusqueda} />
+        </div>
 
-          <br/>
+        <br />
 
-          <div className="jumbotron">
+        <div className="jumbotron">
           <div className="row content-center">
             <Resultado
               imagenes={this.state.imagenes}
@@ -98,37 +98,28 @@ class App extends Component {
           </div>
         </div>
       </div>
-
     );
   }
 }
 
-function app () {
+function app() {
   const signInWithGoogle = async () => {
-
-
     // Coloca el lenguaje de preferencia del dispositivo
     firebase.auth().useDeviceLanguage();
-    
+
     // Inicio el procedo de login dentro de un try ... catch
-    
+
     try {
-    await firebase.auth().signInWithPopup(googleAuthProvider);
-    
+      await firebase.auth().signInWithPopup(googleAuthProvider);
     } catch (e) {
-    
-    console.error(e.message);
-    
+      console.error(e.message);
     }
-    return(
-    <div>
-      <Button onClick={signInWithGoogle} > Sing in with Google</Button>
-    </div> 
+    return (
+      <div>
+        <Button onClick={signInWithGoogle}> Sing in with Google</Button>
+      </div>
     );
-     
-    }
+  };
 }
-
-
 
 export default App;
