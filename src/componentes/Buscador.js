2ee20f3f7eaafe633ebd4a8db99da19e82bc148d
Esterlyn <diazesterlin62@gmail.com>
2021-04-29 22:45:36 -0400

    cargar imagenes por defecto

--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -5,22 +5,27 @@ class Buscador extends Component{
     busquedaRef = React.createRef();
 
     obtenerDatos = (e) => {
-        e.preventDefault();
+        if (e) {
+            e.preventDefault();
+        }
 
         // Tomamos el valor del input
         const termino = this.busquedaRef.current.value;
 
         // Lo enviamos al componente principal
-        this.props.datosBusqueda(termino);
+        this.props.datosBusqueda(termino);    
+    };
 
+    componentDidMount(){
+        this.obtenerDatos();
     }
-
+    
     render(){
         return(
             <form onSubmit={this.obtenerDatos}>
-                <div className="row">
+                <div className="row btnB">
                     <div className="from-group col-md-8">
-                        <input ref= {this.busquedaRef} type="text" className="form-control form-control-lg" 
+                        <input id='buscador' ref= {this.busquedaRef} type="text" className="form-control form-control-lg" 
                         placeholder="Busca tu imagen. Ejemplo: Luna" />
                     </div>
 
