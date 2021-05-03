7837351db8477516660b3216c29d82888fc24b9d
Esterlyn <diazesterlin62@gmail.com>
2021-05-03 12:19:27 -0400

    correciones

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,27 +1,21 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAuUoJ4islSKVPtEF2aYlhZESganqdCTMc",
+  authDomain: "bd-del-login.firebaseapp.com",
+  projectId: "bd-del-login",
+  storageBucket: "bd-del-login.appspot.com",
+  messagingSenderId: "478138430284",
+  appId: "1:478138430284:web:e3a438d9b841ab067d15e9",
+  measurementId: "G-1ZFR7WGJCS",
+};
 
-
-      const firebaseConfig = {
-    apiKey: "AIzaSyAuUoJ4islSKVPtEF2aYlhZESganqdCTMc",
-    authDomain: "bd-del-login.firebaseapp.com",
-    projectId: "bd-del-login",
-    storageBucket: "bd-del-login.appspot.com",
-    messagingSenderId: "478138430284",
-    appId: "1:478138430284:web:e3a438d9b841ab067d15e9",
-    measurementId: "G-1ZFR7WGJCS"
-  };
-
-
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore;
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-  
-export {
-    db,
-    googleAuthProvider,
-    firebase
-}
+
+export { db, googleAuthProvider, firebase };
