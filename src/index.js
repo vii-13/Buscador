c1402dee92c06b428166e2a4f72c84dc1838557c
Esterlyn <diazesterlin62@gmail.com>
2021-05-03 00:00:02 -0400

    correciones

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Subir from './componentes/Subir';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Subir from "./componentes/Subir";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Route exact path='/Buscador' component={App}/>
-      <Route exact path='/subir' component={Subir}/>
-      <Route exact path='/home' component={App}/>
+      <Route exact path="/Buscador" component={App} />
+      <Route exact path="/subir" component={Subir} />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
