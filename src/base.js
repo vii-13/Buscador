843f8593a11e5565847b98c01410641e6c7b47bf
Esterlyn <diazesterlin62@gmail.com>
2021-05-07 16:30:45 -0400

    conexiones firebase

---
+++ src/base.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+import 'firebase/storage'
+
+export const fire = firebase.initializeApp({
+  "projectId": "bd-del-login",
+  "appId": "1:478138430284:web:e3a438d9b841ab067d15e9",
+  "storageBucket": "bd-del-login.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyAuUoJ4islSKVPtEF2aYlhZESganqdCTMc",
+  "authDomain": "bd-del-login.firebaseapp.com",
+  "messagingSenderId": "478138430284",
+  "measurementId": "G-1ZFR7WGJCS"
+});
+
