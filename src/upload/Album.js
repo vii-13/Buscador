c936126e25a28c878f5e9a49453c40e9f69a4aa4
Esterlyn <diazesterlin62@gmail.com>
2021-05-09 01:20:21 -0400

    css

--- src/upload/Album.js
+++ src/upload/Album.js
@@ -48,11 +48,29 @@ export const Album = () => {
           <h1 className="border albumName">{albumName}</h1>
         </header>
         {images.map((image, ind) => (
-          <aside key={image.name + ind}>
-            <a href={image.url} target="blank">
-              <img src={image.url} alt="album" />
-            </a>
-          </aside>
+          <div className="card" key={image.name + ind}>
+            <img src={image.url} alt="album" />
+            <div className="ver">
+              <a
+                href={image.url}
+                target="_blank"
+                className="btn btn-danger
+                    btn-block rounded-pill"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-eye-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                  <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                </svg>
+              </a>
+            </div>
+          </div>
         ))}
       </section>
     </>
